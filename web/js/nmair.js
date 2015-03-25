49119e6f2b01ab7b01f10256c87662eb2b7cd757
Colby Gutierrez-Kraybill <colby@kraybill.com>
2015-03-25 00:10:57 -0600

    payment integrated

--- web/js/nmair.js
+++ web/js/nmair.js
@@ -168,19 +168,3 @@ function resizeDiv(rows, paddedItemSize, columnWrap) {
 		seatingDiagram.commitTransaction("resize div");
 }
 	  
-
-window.onload = function () {
-    $('#depicker').datetimepicker();
-    $('#repicker').datetimepicker();
-
-    $("#depicker").on("dp.change",function (e) {
-      $('#repicker').data("DateTimePicker").minDate(e.date);
-      });
-    $("#repicker").on("dp.change",function (e) {
-      $('#depicker').data("DateTimePicker").maxDate(e.date);
-      });
-   // $('#depicker').data("DateTimePicker").minDate(moment());
-   // $('#repicker').data("DateTimePicker").minDate(moment());
-    //initSeating();
-}
-
