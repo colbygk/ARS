624156cd4f0bc6047bd103bbe87e16b95f44a3b1
Colby Gutierrez-Kraybill <colby@kraybill.com>
2015-04-04 18:57:31 -0600

    helper script in javascript to convert json to sql statements

---
+++ web/convert_json_to_sql.js
@@ -0,0 +1,23 @@
+
+// Imports ./flights.json and ./airports.json
+var flights = require('./flights');
+var airports = require('./airports');
+
+// Important to add the airports first
+// to satisfy foreign key constraints
+// from flights->airports
+var insert_str;
+airports.forEach( function(e) {
+    insert_str = "insert into airports " +
+    "(short_name,long_name) values ('"+e.code+"','"+escape(e.desc)+"');";
+    console.log(insert_str);
+    });
+
+flights.forEach( function(e) {
+    insert_str = "insert into flights " +
+     "(id_str,depart_airport,depart_time,arrive_airport,arrive_time) values ('" +
+     e.id+"',(select id from airports where short_name='"+e.origin+"'),'"+e.depart+
+     "',(select id from airports where short_name='"+e.destination+"'),'"+e.arrive+
+     "');";
+     console.log(insert_str);
+    });
