6d22537f28bfb295be3521c8b0bbe349e481c363
Colby Gutierrez-Kraybill <colby@kraybill.com>
2015-04-04 19:21:44 -0600

    proper use of 24 hour time

--- web/convert_json_to_sql.js
+++ web/convert_json_to_sql.js
@@ -17,8 +17,8 @@ flights.forEach( function(e) {
     insert_str = "insert into flights " +
      "(id_str,depart_airport,depart_time,arrive_airport,arrive_time) values ('" +
      e.id+"',(select id from airports where short_name='"+e.origin+"'),STR_TO_DATE('"+e.depart+
-     ":00','%m/%d/%Y %h:%i'),(select id from airports where short_name='"+e.destination+"'),"+
-     "STR_TO_DATE('"+e.arrive+":00','%m/%d/%Y %h:%i'));";
+     ":00','%m/%d/%Y %H:%i'),(select id from airports where short_name='"+e.destination+"'),"+
+     "STR_TO_DATE('"+e.arrive+":00','%m/%d/%Y %H:%i'));";
 
      console.log(insert_str);
     });
